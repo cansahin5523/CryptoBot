c78633a93383aa70433f01295da07cbc7413b176
cansahin5523 <72552107+cansahin5523@users.noreply.github.com>
2022-05-16 17:52:30 +0300

    Update binanceApi.py

--- binanceApi.py
+++ binanceApi.py
@@ -1,19 +1,33 @@
 from binance.client import Client
 
-apiKey = *
-secretKey = *
+apiKey = 'DbG9kmsCcshJ5tH1UNNsJ8RfePkbTi47cuwzkfACouM9R9kqaAUXI5ash4MKGp4D'
+secretKey = 'cbBbSCigIUyff0dWlTnh9FBOU4C50K2kQBqSE1WGSaLTw7hqHyoUuZ2xufZxvmZW'
 
 client = Client(api_key=apiKey,api_secret=secretKey)
 
 def wallet():
-    balance = client.get_asset_balance("TRX")
-    print(balance,type(balance))
+    balance = client.get_asset_balance("TRY")
+    print("Coin Adi: ",balance["asset"],"\n",
+        "Deger: ",balance["free"])
 
-def buyMarketPrice(cryptoname):
-    client.order_market_buy(cryptoname)
 
-def sellMarketPrice(cryptoname):
-    client.order_market_sell(cryptoname)
+def buyMarketPrice(cryptoName):
+    balance = client.get_asset_balance("TRY")
+    order = client.create_order(
+    symbol=cryptoName,
+    side=client.SIDE_BUY,
+    type=client.ORDER_TYPE_MARKET,
+    quantity=10)
+    print("Complated!")
+
+def sellMarketPrice(cryptoName):
+    balance = client.get_asset_balance("TRY")
+    order = client.create_order(
+    symbol=cryptoName,
+    side=client.SIDE_SELL,
+    type=client.ORDER_TYPE_MARKET,
+    quantity=10)
+    print("Complated!")
+
     
-
 
