9494ec205737bdfa6a5450bc307dadc1660107e5
cansahin5523 <72552107+cansahin5523@users.noreply.github.com>
2022-06-02 16:10:47 +0300

    Update binanceApi.py

--- binanceApi.py
+++ binanceApi.py
@@ -7,27 +7,45 @@ client = Client(api_key=apiKey,api_secret=secretKey)
 
 def wallet():
     balance = client.get_asset_balance("TRY")
-    print("Coin Adi: ",balance["asset"],"\n",
+    print("Varlık Adi: ",balance["asset"],"\n",
         "Deger: ",balance["free"])
 
 
-def buyMarketPrice(cryptoName):
+def buyMarketPrice(tutar):
     balance = client.get_asset_balance("TRY")
     order = client.create_order(
-    symbol=cryptoName,
+    symbol="TRXTRY",
     side=client.SIDE_BUY,
     type=client.ORDER_TYPE_MARKET,
-    quantity=10)
+    quantity=tutar)
     print("Complated!")
 
 def sellMarketPrice(cryptoName):
-    balance = client.get_asset_balance("TRY")
     order = client.create_order(
     symbol=cryptoName,
     side=client.SIDE_SELL,
-    type=client.ORDER_TYPE_MARKET,
-    quantity=10)
+    type=client.ORDER_TYPE_MARKET, 
+    quantity=11)
     print("Complated!")
 
-    
+def sellLimitOrder(cryptoName,satis,sellTimeApi):
+    order = client.create_order(
+        symbol = cryptoName,
+        side = client.SIDE_SELL,
+        type = client.ORDER_TYPE_LIMIT,
+        timeInForce = client.TIME_IN_FORCE_GTC,
+        quantity = satis-1,
+        price = sellTimeApi,  
+    )
+
+def stopLoss(cryptoName,price,stopPrice):
+    order = client.create_order(
+        symbol=cryptoName,
+        side = client.SIDE_SELL,
+        type = client.ORDER_TYPE_STOP_LOSS_LIMIT,
+        timeInForce = client.TIME_IN_FORCE_GTC,
+        quantity = 100,
+        price = price,
+        stopPrice = stopPrice
+    )
 
